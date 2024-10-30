195df300b9f092e38e6a769df23dc1d6ec9ad2cf
xdlhzdh <xdlhzdh@163.com>
2024-10-30 17:46:44 +0800

    optimize exception handling

--- tests/extendedOp/ldap_server.py
+++ tests/extendedOp/ldap_server.py
@@ -15,12 +15,13 @@ from ldap3.protocol.rfc4511 import (
 from pyasn1.codec.ber import decoder, encoder
 from pyasn1.error import PyAsn1Error
 
+
 class LDAPServer:
     def __init__(self):
         # Create TCP socket
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Bind to a random port
-        self.server_socket.bind(("127.0.0.1", 0))
+        self.server_socket.bind(("127.0.0.1", 1234))
         # Get the port number
         self.port = self.server_socket.getsockname()[1]
         # Listen for incoming connections
@@ -30,7 +31,7 @@ class LDAPServer:
 
         # Store client sockets and their SSL objects
         self.connections = []
-        self.requested_data = b""
+        self.request_data = b""
         self.running = True
 
     def get_port(self):
@@ -44,50 +45,59 @@ class LDAPServer:
 
     def start(self):
         try:
-            times = 0
+            ber_req_times = 0
             while self.running:
                 # Use select to wait for connections and readable events
                 readable, _, _ = select.select(
                     [self.server_socket] + self.connections,
-                    [self.server_socket] + self.connections,
+                    [],
                     [self.server_socket] + self.connections,
                     1,
                 )
-                connection_num = 0
                 length = None
                 for sock in readable:
                     if sock is self.server_socket:
                         # Handle new connection
-                        connection_num += 1
                         client_socket, addr = self.server_socket.accept()
                         client_socket.setblocking(False)
                         print(f"New connection from {addr}: {client_socket}")
                         self.connections.append(client_socket)
                     else:
                         # Handle data from existing connection
-                        times = times + 1
-                        if times >= 2:
+                        if ber_req_times == 1:
                             continue
-                        received_data, remaining_len = self.recv_ldap_message(sock, length)
-                        length = remaining_len
-                        if received_data is not None and len(received_data) > 0:
-                            self.requested_data += received_data
-                        print(
-                            f"received_data length = {len(received_data)}, remaining data length = {remaining_len}, \
-requested_data length = {len(self.requested_data)}"
-                        )
-                        if remaining_len == 0 and len(self.requested_data) > 0:
-                            print(f"start decode: {self.requested_data}")
-                            try:
-                                ldap_message, _ = decoder.decode(
-                                    self.requested_data, asn1Spec=LDAPMessage()
+                        try:
+                            received_data, remaining_len = self.recv_ldap_message(
+                                sock, length
+                            )
+                            length = remaining_len
+                            if received_data is not None and len(received_data) > 0:
+                                self.request_data += received_data
+                                print(
+                                    f"received_data length = {len(received_data)}, remaining data length = {remaining_len}, \
+request_data length = {len(self.request_data)}"
                                 )
+                            if remaining_len == 0 and len(self.request_data) > 0:
+                                print(f"start decode: {self.request_data}")
+                                ldap_message, _ = decoder.decode(
+                                    self.request_data, asn1Spec=LDAPMessage()
+                                )
+                                ber_req_times = ber_req_times + 1
+                                print(f"ber_req_times = {ber_req_times}")
                                 print(ldap_message.prettyPrint())
                                 # Check if it is a StartTLS request
-                                message_id = int(ldap_message.getComponentByName("messageID"))
-                                protocol_op = ldap_message.getComponentByName("protocolOp")
-                                extended_req = protocol_op.getComponentByName("extendedReq")
-                                request_name = extended_req.getComponentByName("requestName")
+                                message_id = int(
+                                    ldap_message.getComponentByName("messageID")
+                                )
+                                protocol_op = ldap_message.getComponentByName(
+                                    "protocolOp"
+                                )
+                                extended_req = protocol_op.getComponentByName(
+                                    "extendedReq"
+                                )
+                                request_name = extended_req.getComponentByName(
+                                    "requestName"
+                                )
                                 if request_name is not None:
                                     request_name_str = str(request_name)
                                     print(f"requestName: {request_name_str}")
@@ -101,21 +111,27 @@ requested_data length = {len(self.requested_data)}"
                                     print("Received invalid LDAP message.")
                                     # Handle other LDAP messages
 
-                            except LDAPException as e:
-                                print(f"LDAP Exception: {e}")
+                        except LDAPException as e:
+                            print(f"LDAP Exception: {e}")
 
-                            except PyAsn1Error as e:
-                                print(f"Failed to decode LDAP message: {e}")
-                        else:
-                            # Close connection
-                            print(f"No data received from {sock}")
-                            sock.close()
-                            self.connections.remove(sock)
+                        except PyAsn1Error as e:
+                            print(f"Failed to decode LDAP message: {e}")
+
+                        except Exception:
+                            pass
+
+                        # else:
+                        #     # Close connection
+                        #     print(f"No data received from {sock}")
+                        #     sock.close()
+                        #     self.connections.remove(sock)
 
         finally:
             self.stop()
 
-    def construct_extended_response(self, message_id, result_code, response_name=None, response_value=None):
+    def construct_extended_response(
+        self, message_id, result_code, response_name=None, response_value=None
+    ):
         # Create LDAPMessage object
         ldap_message = LDAPMessage()
 
@@ -195,7 +211,9 @@ requested_data length = {len(self.requested_data)}"
             if first_length_byte & 0x80 == 0:  # Short form
                 message_length = first_length_byte
             else:  # Long form
-                num_length_bytes = first_length_byte & 0x7F  # Get the number of bytes representing the length
+                num_length_bytes = (
+                    first_length_byte & 0x7F
+                )  # Get the number of bytes representing the length
                 length_bytes = sock.recv(num_length_bytes)  # Read the length bytes
                 if not length_bytes or len(length_bytes) != num_length_bytes:
                     raise Exception("Invalid length encoding or connection closed")
@@ -235,6 +253,7 @@ requested_data length = {len(self.requested_data)}"
 
         # Return the complete Tag + Length + Value
         return tlv_data + message_data, 0
+
 
 if __name__ == "__main__":
     # Example usage
